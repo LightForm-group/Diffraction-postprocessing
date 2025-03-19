62761c3cf74cf8be09b415aa6a17bb3baaa95e2c
GBowker <guy.bowker@postgrad.manchester.ac.uk>
2025-03-19 17:11:02 +0000

    add ODF generator and importer

--- Texture_preprocessing/generate_odf.m
+++ Texture_preprocessing/generate_odf.m
@@ -4,26 +4,29 @@ addpath '/Users/user/Documents/MATLAB/mtex-5.7.0'
 startup_mtex
 clear all
 
-%% === load phases: === %
+%% === load phase Crystal Symmetry: === %
 % change to 'Ti64_alpha' or 'Ti64_beta'
 
 % Ti64_alpha conventions
 % DAMASK: X||a Y||b*
 % Rolling: X||a* Y||b
 % Chris uses X||b* Y||a Z||c*
-cs = crystalSymmetry('6/mmm', [3 3 4.7], 'X||a', 'Y||b*', 'Z||c', 'mineral', 'Ti-Hex', 'color', [0.53 0.81 0.98]);
-% odf.SS=specimenSymmetry('triclinic');
-hkil = [Miller(0,0,0,2,cs), Miller(1,0,-1,0,cs), Miller(1,1,-2,0,cs)];
+
+% cs = crystalSymmetry('6/mmm', [3 3 4.7], 'X||a', 'Y||b*', 'Z||c', 'mineral', 'Ti-Hex', 'color', [0.53 0.81 0.98]);
+% % odf.SS=specimenSymmetry('triclinic');
+% hkil = [Miller(0,0,0,2,cs), Miller(1,0,-1,0,cs), Miller(1,1,-2,0,cs)];
 
 % Ti64_beta conventions
-% cs = crystalSymmetry('m-3m', [3.2 3.2 3.2], 'mineral', 'Titanium cubic', 'color', [0.56 0.74 0.56]);
+cs = crystalSymmetry('m-3m', [3.2 3.2 3.2], 'mineral', 'Titanium cubic', 'color', [0.56 0.74 0.56]);
 % odf.SS=specimenSymmetry('orthorombic');
-% hkil = [Miller(0,0,1,cs), Miller(1,1,0,cs), Miller(1,1,1,cs)];
+hkil = [Miller(0,0,1,cs), Miller(1,1,0,cs), Miller(1,1,1,cs)];
+
 
 % Al FCC conventions
 % cs = crystalSymmetry('m-3m', [3.2 3.2 3.2], 'mineral', 'Titanium cubic', 'color', [0.56 0.74 0.56]);
 % odf.SS=specimenSymmetry('orthorombic');
 % hkil = [Miller(0,0,1,cs), Miller(1,1,0,cs), Miller(1,1,1,cs)];
+
 
 % W BCC conventions
 % cs = crystalSymmetry('m-3m', [3.2 3.2 3.2], 'mineral', 'Titanium cubic', 'color', [0.56 0.74 0.56]);
@@ -40,11 +43,12 @@ eulers = [
 %     [0.888 0.0 0.325 -0.325] % {111}
 %     ];
 
+% convert orientations to matlab quaterions...
 q = euler2quat(eulers(1,1)*degree,eulers(1,2)*degree,eulers(1,3)*degree,'Bunge'); % converted from bunge
 disp(q)
 % q = quaternion(transpose(quat)); % defined from quat
 
-% Estimate an ODF from the orientations
+%% Estimate an ODF from the orientations...
 % calcdensity method
 ori = orientation(q, cs);
 odf = calcDensity(ori, 'halfwidth', 0.2);
@@ -53,7 +57,12 @@ IPFkey = ipfColorKey(odf.CS);
 IPFkey.inversePoleFigureDirection = vector3d.(upper('Z')); % define direction for IPF colours
 oriColors = IPFkey.orientation2color(ori);
 
-%% plot pf...
+%% plot 3D ODF...
+newMtexFigure('figSize', 'large')
+plot3d(odf)
+
+
+%% plot pole figures...
 newMtexFigure('figSize', 'large', 'layout', [1,3])
 setMTEXpref('FontSize', 32);
 
@@ -63,8 +72,8 @@ setMTEXpref('zAxisDirection','OutofPlane');
 
 % plot
 mrd_max = 3;
-% plotPDF(odf, hkil,'antipodal', 'contourf', 0:0.25:mrd_max, 'minmax') % plot with contouring
-plotPDF(ori, hkil, 'antipodal', 'property', oriColors, 'minmax'); % plot IPF color points
+plotPDF(odf, hkil,'antipodal', 'contourf', 0:0.25:mrd_max, 'minmax') % plot with contouring
+% plotPDF(ori, hkil, 'antipodal', 'property', oriColors, 'minmax'); % plot IPF color points
 
 % Nicer annotations
 text(vector3d.X,'X','VerticalAlignment','bottom'); % moving the vector3d axis labels outside of the hemisphere boundary
@@ -81,4 +90,4 @@ end
 
 %% save odf...
 
-save('ODF.mat', 'odf');
+save('ODF_0002ND1010RD.mat', 'odf');
